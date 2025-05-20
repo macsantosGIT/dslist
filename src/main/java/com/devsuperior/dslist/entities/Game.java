c8e03fde0d469e665890842a8426cd97fe8fb689
macsantosGIT <marcelo.correa.santos@gmail.com>
2025-05-19 22:17:01 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -19,22 +19,30 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
+
+    @Column(name = "img_url")
     private String imgUrl;
+
+    @Column(name = "short_description", columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(name = "long_description", columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
         
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -72,14 +80,21 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
     public String getImgUrl() {
         return imgUrl;
     }
@@ -129,6 +144,5 @@ public class Game {
         return true;
     }
 
-    
 
 }
